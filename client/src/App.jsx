5ef76217f6d49bb935d9509a85f9569c2e23c43c
Marco Sarmiento <marco.sarmiento1120@gmail.com>
2021-09-03 04:03:29 -0700

    completed form_generator prototype

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
 import EmployeeCreate from './components/employees/employee_new';
+import FormGenerator from './components/form_generator';
 import styles from './styles/styles.css';
+
+const EmployeeNewForm = [
+    {
+        formGroup: {
+            name: "Personal Information",
+            inputs: [
+                {
+                    inputGroup:
+                        [
+                            { label: "First Name", id: "firstName", type: "text", placeholder: "Jane" },
+                            { label: "Last Name", id: "lastName", type: "text", placeholder: "Doe" }
+                        ]
+                },
+                {
+                    inputGroup: 
+                    [
+                        { label: "Username", id: "username", type: "text", placeholder: "User" }
+                    ]
+                },
+                {
+                    inputGroup:
+                        [
+                            { label: "Password", id: "password", type: "password", placeholder: "Password" }
+                        ]
+                },
+                {
+                    inputGroup:
+                    [
+                        { label: "Address", id: "address", type: "text", placeholder: "123 Palm Lane"}
+                    ]
+                }
+            ]
+        },
+    }
+];
 
 const app = () => {
     return (
         <>
             <div className=" w-full h-screen">
-                <EmployeeCreate />
+                <FormGenerator object={EmployeeNewForm} />
             </div>
         </>
     );
--- client/src/components/form_generator.jsx
+++ client/src/components/form_generator.jsx
@@ -1,35 +1,37 @@
 import React, { Component } from 'react';
 
-let FormGenerator = (formObject) => {
+let FormGenerator = (object) => {
 
-    let GenerateForm = (formInput, inputs = {}) => {
-        formInput.map((input, i) => {
-            if (input.formGroup) return renderFormGroup(input.formGroup);
-            if (input.inputGroup) return renderInputGroup(input.inputGroup);
+    let formObject = object.object;
+
+    let GenerateForm = (formInput) => {
+        return formInput.map((input, i) => {
+            if (input.formGroup) return renderFormGroup(input.formGroup, i);
+            if (input.inputGroup) return renderInputGroup(input.inputGroup, i);
             return renderInput(input, inputs);
         })
     }
 
-    let renderFormGroup = (formGroup) => {
+    let renderFormGroup = (formGroup, i) => {
         return (
             <>
                 <div className="flex flex-wrap w-full border-b-2 pb-2">
                     <p key={i} className="font-semibold text-2xl">{formGroup.name}</p>
                 </div>
-                {GenerateForm(formGroup.inputs)}
+                { GenerateForm(formGroup.inputs) }
             </>
         );
     }
 
-    let renderInputGroup = (inputGroup) => {
-        let ig = inputGroup.map((input, i) => {
-            renderInput(input);
+    let renderInputGroup = (inputGroup, i) => {
+        return inputGroup.map((input, i) => {
+            return renderInput(input, inputGroup.length, i);
         })
     }
 
-    let renderInput = (input) => {
+    let renderInput = (input, length, i) => {
         let conSel = () => {
-            if (input.length > 1) {
+            if (length > 1) {
                 return (i == 0) ? "w-full md:w-1/2 px-3 mb-6 md:mb-0" : "w-full md:w-1/2 px-3"
             } else return "w-full px-3 mb-6 md:mb-0"
         }
@@ -40,13 +42,13 @@ let FormGenerator = (formObject) => {
                     <label
                         className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                         htmlFor={input.id}>
-                        {forms.label}
+                        {input.label}
                     </label>
                     <input
                         className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id={forms.id}
-                        type={forms.type}
-                        placeholder={forms.placeholder} />
+                        id={input.id}
+                        type={input.type}
+                        placeholder={input.placeholder} />
                 </div>
             </>
         )
@@ -61,3 +63,5 @@ let FormGenerator = (formObject) => {
     )
 
 };
+
+export default FormGenerator;
